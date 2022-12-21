17d045d702d6b26facec7cc52148c780501d59a3
Matt Hickling <m.hickling@hotmail.co.uk>
2022-12-21 11:25:19 +0000

    App finished

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
 
+import {BrowserRouter as Router} from 'react-router-dom'
+import { Route, Switch, } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
+      <Switch>
+      <Route path='/pet-details-not-found'>
+      <PetDetailsNotFound/>
+      </Route>
+      <Route path="/:type/:id">
+        <PetDetailsPage/>   
+      </Route>
+      <Route path='/search'>
+      <SearchPage />
+      </Route>
+      <Route path='/:type?'>
       <HomePage />
-    </div>
+      </Route>
+      </Switch>
+     
+    </Router>
   );
 }
 
